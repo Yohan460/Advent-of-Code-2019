c2f1ff7f678929137c8168551686042598b1c322
Johan McGwire <johan@mcgwire.tech>
2019-12-02 13:49:01 -0500

    Day 1 complete

--- 1/Day 1.playground/Contents.swift
+++ 1/Day 1.playground/Contents.swift
@@ -1,3 +1,16 @@
 import Cocoa
 
 var str = "Hello, playground"
+let inputURL = Bundle.main.path(forResource: "input", ofType: "txt")
+let inputFile = FileManager.default.contents(atPath: inputURL!)
+let input = String(data:inputFile!, encoding:String.Encoding.utf8)?.components(separatedBy: "\n")
+var output = [Double].init()
+
+for massStr in input! {
+    var fuel = ((Double(massStr) ?? 8) / 3.0)
+    fuel.round(.towardZero)
+    fuel = fuel - 2.0
+    output.append(fuel)
+}
+
+print(output.reduce(0, +))
